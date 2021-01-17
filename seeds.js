f9b9a1f221eb56bc5c554191e969e3840cf1e609
Steven McCarther <smccar5676@aol.com>
2021-01-17 14:13:20 -0500

    modified seeds.js to add additional type of burgers

---
+++ seeds.js
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Smokehouse');
+INSERT INTO burgers (name) VALUES ('MeanBean');
+INSERT INTO burgers (name) VALUES ('Veggieornot?');
+INSERT INTO burgers (name) VALUES ('Turkeyjerk');
+INSERT INTO burgers (name) VALUES ('Goudafoyou');
